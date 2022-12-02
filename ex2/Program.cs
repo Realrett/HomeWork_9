ad153195704b78a1a3c4946f83db3d8cdadd5acd
Realrett <gosha-student@mail.ru>
2022-12-02 19:38:27 +0700

    2 задача

---
+++ ex2/Program.cs
@@ -0,0 +1,27 @@
+﻿/* Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных 
+элементов в промежутке от M до N.
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30
+*/ 
+
+int GetNumber(string msg)
+{
+    Console.WriteLine(msg);
+    int userNumber = Convert.ToInt32(Console.ReadLine());
+    return userNumber;
+}
+
+void PrintNumbers(int number, int count, int sum)
+{
+    if (count <= number)
+    {
+        sum = sum + count;
+        PrintNumbers(number, count + 1, sum);
+    }
+    else Console.WriteLine(sum);
+}
+
+int numberM = GetNumber("Введите число M:");
+int numberN = GetNumber("Введите число N:");
+int summ = 0;
+PrintNumbers(numberN, numberM, summ);
